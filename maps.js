2a8e2eedaa96512d6eddad5ca44d6413e07d65af
J M <wohaste@gmail.com>
2021-01-10 10:24:30 -0700

    change map size

--- maps.js
+++ maps.js
@@ -30,6 +30,15 @@ function InitMapContainer(container) {
 	canvas.addEventListener('click', PlaceTile);
 	context = canvas.getContext('2d');
 	container.appendChild(canvas);
+	
+	document.getElementById('add-row-north').addEventListener('click', AddRowNorth);
+	document.getElementById('minus-row-north').addEventListener('click', MinusRowNorth);
+	document.getElementById('add-row-south').addEventListener('click', AddRowSouth);
+	document.getElementById('minus-row-south').addEventListener('click', MinusRowSouth);
+	document.getElementById('add-row-east').addEventListener('click', AddRowEast);
+	document.getElementById('minus-row-east').addEventListener('click', MinusRowEast);
+	document.getElementById('add-row-west').addEventListener('click', AddRowWest);
+	document.getElementById('minus-row-west').addEventListener('click', MinusRowWest);
 }
 
 function DrawMap() {
@@ -57,6 +66,95 @@ function PlaceTile(event) {
 	mapArray[y][x] = selectedTile.fileName;
 	
 	DrawMap();
+}
+
+function AddRowNorth(event) {
+	mapArray.unshift([]);
+	for(let x=0; x<mapArray[1].length; x++)
+	{
+		mapArray[0].push(BLANK);
+	}
+	DrawMap();
+	
+	return false;
+}
+
+function MinusRowNorth(event) {
+	if(GetMapTileHeight() <= initialMapDimension) {
+		return;
+	}
+	mapArray.shift();
+	DrawMap();
+	
+	return false;
+}
+
+function AddRowSouth(event) {
+	mapArray.push([]);
+	let y = GetMapTileHeight() - 1;
+	for(let x=0; x<mapArray[0].length; x++)
+	{
+		mapArray[y].push(BLANK);
+	}
+	DrawMap();
+	
+	return false;
+}
+
+function MinusRowSouth(event) {
+	if(GetMapTileHeight() <= initialMapDimension) {
+		return;
+	}
+	mapArray.pop();
+	DrawMap();
+	
+	return false;
+}
+
+function AddRowWest(event) {
+	for(let y=0; y<GetMapTileHeight(); y++)
+	{
+		mapArray[y].unshift(BLANK);
+	}
+	DrawMap();
+	
+	return false;
+}
+
+function MinusRowWest(event) {
+	if(GetMapTileWidth() <= initialMapDimension) {
+		return;
+	}
+	for(let y=0; y<GetMapTileHeight(); y++)
+	{
+		mapArray[y].shift();
+	}
+	DrawMap();
+	
+	return false;
+}
+
+function AddRowEast(event) {
+	for(let y=0; y<GetMapTileHeight(); y++)
+	{
+		mapArray[y].push(BLANK);
+	}
+	DrawMap();
+	
+	return false;
+}
+
+function MinusRowEast(event) {
+	if(GetMapTileWidth() <= initialMapDimension) {
+		return;
+	}
+	for(let y=0; y<GetMapTileHeight(); y++)
+	{
+		mapArray[y].pop();
+	}
+	DrawMap();
+	
+	return false;
 }
 
 function SetCanvasSize() {
